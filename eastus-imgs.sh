23e0c873bef99de3bff2aa018596a87582c04e6b
Benjamin1976 <bp_coding@yahoo.com.au>
2023-12-02 17:55:16 +0800

    Create eastus-imgs.sh

---
+++ eastus-imgs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Use this for your user data (script from top to bottom)
+# install httpd (Linux 2 version)  
+sudo apt update
+sudo apt install apache2
+echo "<h1>Welcome to Image server for US, $(hostname): $(hostname -f)</h1>" > /var/www/html/index.html 
